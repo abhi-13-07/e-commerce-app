7d65d72f0e2c30dc50d5614054dcecf4acb4fe0a
abhi-13-07 <abhishek.13.july.02@gmail.com>
2021-02-24 17:07:31 +0530

    add initial route and render views

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if (NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config();
 }
 const express = require('express');
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 const expressLayout = require('express-ejs-layouts');
 const session = require('express-session');
 const flash = require('express-flash');
+
+const indexRoutes = require('./routes/index');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -27,13 +29,19 @@ app.use(
 app.use(flash());
 
 (async () => {
-	const { connection } = await mongoose.connect(process.env.DATABASE_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-	});
-	console.log(`Connected To Database(${connection.host}) `);
+	try {
+		const { connection } = await mongoose.connect(process.env.DATABASE_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log(`Connected To Database(${connection.host}) `);
+	} catch (err) {
+		console.log(err.message);
+	}
 })();
+
+app.get('/', indexRoutes);
 
 app.listen(process.env.PORT, () =>
 	console.log(`Server started on port ${process.env.PORT}`)
